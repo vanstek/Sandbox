a98e34d4a7d86a9a0723ecdbff24f29a255e636c
Alex van Stekelenborg <35575349+AlStek@users.noreply.github.com>
2019-12-09 22:33:45 -0500

    score counter and live counter added

--- eyesight.php
+++ eyesight.php
@@ -20,6 +20,9 @@
     <!-- annyang commands -->
     <script>
         var score = 0;
+        var wrong = 0;
+        var size = 3.75;
+        var gameStarted = false;
         var gen_string = words({
             exactly: 3,
             maxLength: 4,
@@ -31,10 +34,9 @@
         function startEyesight() {
             $('#eyesight_desc').remove();
             $('#eyesight-string').append(gen_string);
-            scrolldown = $('#eyeball').offset().top - $('#eyeball').height() / 2;
-            $('html, body').animate({
-                scrollTop: scrolldown
-            });
+            $("#eyesight-counter").css("display", "flex");
+            $(window).scrollTop($('#eyeball').offset().top - ($('.game-header').height() / 10));
+            gameStarted = true;
         }
 
         if (annyang) {
@@ -43,29 +45,11 @@
                 'start game': function() {
                     $('#eyesight_desc').remove();
                     $('#eyesight-string').append();
-
                     $("#eyesight-string").html(gen_string);
-                    scrolldown = $('#eyeball').offset().top - $('#eyeball').height() / 2;
-                    $('html, body').animate({
-                        scrollTop: scrolldown
-                    });
+                    console.log($('#eyeball').offset().top)
+                    $(window).scrollTop($('#eyeball').offset().top - ($('.game-header').height() / 10));
+                    gameStarted = true;
                 },
-
-                'skip': function() {
-                    gen_string = words({
-                        exactly: 3,
-                        maxLength: 12,
-                        join: ' '
-                    }).toUpperCase();
-
-                    $("#eyesight-string").html(gen_string);
-                    scrolldown = $('#eyeball').offset().top + ($('#eyeball').height() / 2);
-                    $('html, body').animate({
-                        scrollTop: scrolldown
-                    })
-
-                },
-
 
 
                 // 'test this': function() {
@@ -97,7 +81,7 @@
         //annyang word printer. checks if correct string is said, then changes text size and text
         annyang.addCallback('result', function(phrases) {
             new_phrases = [];
-            //removes spaces
+            //removes white space from random strings for easier comparison
             for (index = 0; index < phrases.length; index++) {
                 new_phrases.push(phrases[index].replace(/\s+/g, ''));
 
@@ -105,35 +89,113 @@
             console.log("I think the user said: ", phrases[0]);
             console.log("But then again, it could be any of the following: ", phrases);
 
+            if (gameStarted) {
+                gen_string_nospaces = gen_string.replace(/\s+/g, '').toUpperCase();
+                var answer = new_phrases[0].toString().toUpperCase();
+                console.log("question: " + gen_string_nospaces);
+                console.log("answer: " + answer);
+                console.log(answer.localeCompare(gen_string_nospaces));
 
-            gen_string_nospaces = gen_string.replace(/\s+/g, '').toUpperCase();
-            var answer = new_phrases[0].toString().toUpperCase();
-            console.log("question: " + gen_string_nospaces);
-            console.log("answer: " + answer);
-            console.log(answer.localeCompare(gen_string_nospaces));
+
+                // SKIP COMMAND
+                if (answer.localeCompare('SKIP') == 0) {
+                    if ($('#eyesight_desc').length) {
+                        $('#eyesight_desc').remove();
+                    }
+                    console.log("skipped!")
+                    $("#eyesight-string").html('Skipped');
+                    $("#eyesight-string").html();
+                    $("#eyesight-string").change(function() {
+                        $('.eyesight-string').css("font-size", "3.5in");
+                    });
 
 
-            if (answer.localeCompare(gen_string_nospaces) == 0) {
-                console.log("correct!")
-                //generates new string when correct
-                gen_string = words({
-                    exactly: 3,
-                    maxLength: 4,
-                    join: ' '
-                }).toUpperCase();
 
-                $("#eyesight-string").html(gen_string);
-                scrolldown = $('#eyeball').offset().top - $('#eyeball').height() / 2;
-                $('html, body').animate({
-                    scrollTop: scrolldown
-                });
-            } else if (new_phrases[0] !== gen_string) {
-                console.log('wrong answer');
-                scrolldown = $('#eyeball').offset().top - $('#eyeball').height() / 2;
-                $('html, body').animate({
-                    scrollTop: scrolldown
-                });
+                    //generates new string when skipped
+                    gen_string = words({
+                        exactly: 3,
+                        maxLength: 4,
+                        join: ' '
+                    }).toUpperCase();
+
+
+                    setTimeout(
+                        function() {
+                            $("#eyesight-string").html(gen_string);
+                            $('.eyesight-string').css("font-size", size.toString() + "in");
+                        }, 1500);
+
+                    $(window).scrollTop($('#eyeball').offset().top);
+                    $(window).scrollTop($('#eyeball').offset().top - ($('.game-header').height() / 10));
+                }
+
+
+
+                // CORRECT ANSWER CODE
+                else if (answer.localeCompare(gen_string_nospaces) == 0) {
+                    console.log("correct!")
+                    score++;
+                    $("#eyesight-score").html(score);
+
+
+                    size *= .75;
+                    console.log('score: ' + score)
+                    //generates new string when correct
+                    gen_string = words({
+                        exactly: 3,
+                        maxLength: 4,
+                        join: ' '
+                    }).toUpperCase();
+                    var point = ' points.'
+                    if (score == 1) {
+                        point = 'point.';
+                    }
+                    $("#eyesight-string").html('Correct! ' + score.toString() + point);
+                    $("#eyesight-string").change(function() {
+                        $('.eyesight-string').css("font-size", "3.5in");
+                    });
+
+
+                    setTimeout(
+                        function() {
+                            $("#eyesight-string").html(gen_string);
+                            $('.eyesight-string').css("font-size", size.toString() + "in");
+                        }, 3000);
+                    $(window).scrollTop($('#eyeball').offset().top - ($('.game-header').height() / 10));
+
+
+
+                }
+                // WRONG ANSWER CODE
+                else if (new_phrases[0] !== gen_string) {
+                    console.log('wrong answer');
+                    wrong++;
+                    console.log(wrong + ' wrong')
+                    $("#eyesight-string").html('Wrong! ' + (5 - wrong).toString() + ' lives left.');
+                    $("#eyesight-string").html();
+                    $("#eyesight-string").change(function() {
+                        $('.eyesight-string').css("font-size", "3.5in");
+                    });
+                    $("#skull" + wrong.toString()).remove();
+
+
+
+                    gen_string = words({
+                        exactly: 3,
+                        maxLength: 4,
+                        join: ' '
+                    }).toUpperCase();
+
+
+                    setTimeout(
+                        function() {
+                            $("#eyesight-string").html(gen_string);
+                            $('.eyesight-string').css("font-size", size.toString() + "in");
+                        }, 3000);
+                    $(window).scrollTop($('#eyeball').offset().top - ($('.game-header').height() / 10));
+                }
             }
+
         });
     </script>
 
@@ -162,23 +224,34 @@
                 <div class="col-12">
                     <div class="game eyesight-game" id="eyeball">
                         <div class="eyesight-description" id="eyesight_desc">Welcome to the Eyesight Test. This test is not endorsed by doctors, and you should not use this to come to any scientific or physiological conclusions about yourself.<br><br>
-                            <b> Instructions:</b> <br>The game is simple. 5 random letters and numbers will be displayed on screen. It is your duty to recite those letters and numbers back to the computer at a distance of ~10 feet. Make sure you have allowed
-                            the website to access your microphone, or else it won't work. Every time you recite the alphanumeric combination correctly, the text will get smaller, and you will go again. Every correct answer is a point.
-                            You lose when answer incorrectly. The text is ~4 inches tall, but varies with the PPI of your monitor. <br><br>Best of luck!
+                            <b> Instructions:</b> <br>The game is simple. 3 random word will be displayed on screen.. It is your duty to recite those words at a distance of ~10 feet. Make sure you have allowed
+                            the website to access your microphone, or else it won't work. Every time you recite the words correctly, the text will get smaller, and you will go again. Every correct answer is a point, and you get five strikes until you lose.
+                            The text is ~4 inches tall, but varies with the PPI of your monitor. Also, if there are some words that could be homophones, feel free to say "skip" to generate new words. <br><br>Best of luck!
                             <br><br>
                             <center><button class="btn btn-light" onclick="startEyesight()">Start game</button></center>
 
                         </div>
                         <div class="eyesight-string" id="eyesight-string"></div>
+                        <div class="eyesight-counter" id="eyesight-counter">
+                            <div class="eyesight-lives" id="eyesight-lives">
+                                <img alt="skull icon" id="skull1" src="./img/skull.png">
+                                <img alt="skull icon" id="skull2" src="./img/skull.png">
+                                <img alt="skull icon" id="skull3" src="./img/skull.png">
+                                <img alt="skull icon" id="skull4" src="./img/skull.png">
+                                <img alt="skull icon" id="skull5" src="./img/skull.png">
+                            </div>
+                            <div class="eyesight-score">
+                                <p>Points</p>
+                                <div id="eyesight-score">0</div>
+                            </div>
+                        </div>
                     </div>
-
                 </div>
-
-
             </div>
         </div>
     </div>
-    <div class="container-fluid h-25"></div>
+    <div class="container-fluid h-25">
+    </div>
 
 
 
